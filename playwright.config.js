6a23ddeb1339c7a45c6b3893ae32d1d29fe7fb3e
vishnu <vishnuvishwakarma2323@gmail.com>
2026-01-10 15:19:49 +0530

    added

--- playwright.config.js
+++ playwright.config.js
@@ -26,11 +26,11 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  //retries: process.env.CI ? 2 : 0,
-  retries:2,  //Rerun failed test cases
+  retries: process.env.CI ? 2 : 0,
+  //retries:2,  //Rerun failed test cases
   /* Opt out of parallel tests on CI. */
-  //workers: process.env.CI ? 1 : undefined,
-  workers: 4 ,  // Execute test cases in parallel
+  workers: process.env.CI ? 1 : undefined,
+  //workers: 4 ,  // Execute test cases in parallel
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   /*reporter: 'html', */
