e52ad7bfe10cd6715d42938db6f7a7404d30c087
vishnu <vishnuvishwakarma2323@gmail.com>
2026-01-11 17:04:58 +0530

    added new broswer

--- playwright.config.js
+++ playwright.config.js
@@ -70,15 +70,15 @@ reporter:[['allure-playwright',{outputFolder:'allure-results'}]],
        },  
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
